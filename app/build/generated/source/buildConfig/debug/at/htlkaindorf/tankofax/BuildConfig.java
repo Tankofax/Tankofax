7a376bb321f93eff2ba8d6c51c2104d40391f8b8
Matio <matiotawdrous@gmail.com>
2021-04-13 09:59:32 +0200

    test

---
+++ app/build/generated/source/buildConfig/debug/at/htlkaindorf/tankofax/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package at.htlkaindorf.tankofax;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "at.htlkaindorf.tankofax";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
