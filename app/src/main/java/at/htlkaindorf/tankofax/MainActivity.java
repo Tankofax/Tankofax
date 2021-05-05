e2861bceb93d57a0646433e682748806eb0c9f8a
Matio <matiotawdrous@gmail.com>
2021-05-05 12:08:32 +0200

    Position works

--- app/src/main/java/at/htlkaindorf/tankofax/MainActivity.java
+++ app/src/main/java/at/htlkaindorf/tankofax/MainActivity.java
@@ -1,8 +1,17 @@
 package at.htlkaindorf.tankofax;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,24 +20,58 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
-    private GoogleMap map;
+import java.util.Objects;
 
+public class MainActivity extends AppCompatActivity implements LocationListener, OnMapReadyCallback {
+    private GoogleMap map;
+    private static final int REQUEST_LOCATION = 1;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private double lat, lon;
+    private SupportMapFragment mapFragment;
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) this);
+    }
+
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        lat = location.getLatitude();
+        lon = location.getLongitude();
+        Log.d("Position", lat + "," + lon);
         mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("Latitude","status");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d("Latitude","enable");
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d("Latitude","disable");
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-        LatLng sydney = new LatLng(-34, 151);
+        LatLng currentPosition = new LatLng(lat, lon);
         map.addMarker(new MarkerOptions()
-                .position(sydney)
-                .title("Marker in Sydney"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                .position(currentPosition)
+                .title("Marker at Current Pos"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(currentPosition));
     }
 }
